9ce4e4911e6f14312cdbdfc8ee4ea3a1252b3d66
Tamizhvendan S <tamizh88@gmail.com>
2017-02-24 18:31:15 +0530

    adding badrequest and internalerror

--- App.fsx
+++ App.fsx
@@ -1,6 +1,10 @@
-// I'd like to send "Hello, World!" with the Status Code OK
-// I want it async 
 #load "MiniSuave.fsx"
 open MiniSuave
 
-let myApp = OK "Hello, World!"
+(* That's great. But, I don't have anything to say *)
+//let myApp = NOT_FOUND "I don't have!"
+
+(* Wow! But Something went wrong while processing. 
+   How do I communicate it?
+ *)
+ let myApp = INTERNAL_ERROR "Something went wrong!"
--- MiniSuave.fsx
+++ MiniSuave.fsx
@@ -1,13 +1,19 @@
-type StatusCode = OK
+type StatusCode = OK | NotFound | InternalError
 
 type HttpResult = {
   Content : string
   StatusCode : StatusCode
 }
 
-let OK content = async {
-  return {Content = content; StatusCode = OK}
+let response statusCode content = async {
+  return {Content = content; StatusCode = statusCode}
 }
+
+let OK = response OK
+
+let NOT_FOUND = response NotFound
+
+let INTERNAL_ERROR = response InternalError
 
 let execute app = 
   app

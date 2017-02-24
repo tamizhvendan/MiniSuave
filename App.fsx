b4011cbc45af725400a0aee7f0aca22377f99dc3
Tamizhvendan S <tamizh88@gmail.com>
2017-02-24 18:52:40 +0530

    adding filter

--- App.fsx
+++ App.fsx
@@ -1,10 +1,8 @@
 #load "MiniSuave.fsx"
 open MiniSuave
 
-(* That's great. But, I don't have anything to say *)
-//let myApp = NOT_FOUND "I don't have!"
+(*
+  I'd like to say "Hello GET" with the StatusCode Ok for all my get requests
+*)
 
-(* Wow! But Something went wrong while processing. 
-   How do I communicate it?
- *)
- let myApp = INTERNAL_ERROR "Something went wrong!"
+let myApp = filter Get (OK "Hello GET")
--- MiniSuave.fsx
+++ MiniSuave.fsx
@@ -1,4 +1,9 @@
-type StatusCode = OK | NotFound | InternalError
+type StatusCode = Ok | NotFound | InternalError
+type HttpMethod = Get
+
+type HttpRequest = {
+  HttpMethod : HttpMethod
+}
 
 type HttpResult = {
   Content : string
@@ -9,11 +14,17 @@ let response statusCode content = async {
   return {Content = content; StatusCode = statusCode}
 }
 
-let OK = response OK
+let OK = response Ok
 
 let NOT_FOUND = response NotFound
 
 let INTERNAL_ERROR = response InternalError
+
+let filter request app = async {
+  match request with
+  | Get -> return! app
+}
+   
 
 let execute app = 
   app

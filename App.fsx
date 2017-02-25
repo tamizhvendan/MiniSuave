e7a324b3a6669baf334df545191ab78ba9e37f2b
Tamizhvendan S <tamizh88@gmail.com>
2017-02-25 11:10:42 +0530

    adding path filter

--- App.fsx
+++ App.fsx
@@ -2,6 +2,4 @@
 open MiniSuave
 
 (* I'd like to set MimeType *)
-let myApp = GET (OK "Hello PUT")
-
-// http://bit.ly/fs_value_restriction
+let myApp = Path "/hello" (GET (OK "Hello GET"))
--- MiniSuave.fsx
+++ MiniSuave.fsx
@@ -3,6 +3,7 @@ type HttpMethod = Get | Post | Put
  
 type HttpRequest = {
   HttpMethod : HttpMethod
+  Path : string
 }
 
 type HttpResponse = {
@@ -18,16 +19,16 @@ let response statusCode content (req : HttpRequest)  = async {
 let OK = response Ok
 let NOT_FOUND = response NotFound
 let INTERNAL_ERROR = response InternalError
-let httpMethodFilter httpMethod app request : Async<HttpResponse option> = async {
-  match request.HttpMethod = httpMethod with
+let httpMethodFilter iff app request : Async<HttpResponse option> = async {
+  match iff request with
   | true -> return! app request
   | _ -> return None
 }
   
-let GET = httpMethodFilter Get
-let POST = httpMethodFilter Post  
-let PUT = httpMethodFilter Put 
-  
+let GET = httpMethodFilter (fun request -> request.HttpMethod = Get)
+let POST = httpMethodFilter (fun request -> request.HttpMethod = Post)  
+let PUT = httpMethodFilter (fun request -> request.HttpMethod = Put) 
+let Path path  = httpMethodFilter (fun request -> request.Path = path)  
 let execute (app : Handler) req = 
   let res = app req |> Async.RunSynchronously
   match res with

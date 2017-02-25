da171d6cfb14fd3d1b1381afdadb21952ccd16e3
Tamizhvendan S <tamizh88@gmail.com>
2017-02-25 05:43:37 +0530

    adding get,put,post

--- App.fsx
+++ App.fsx
@@ -5,4 +5,7 @@ open MiniSuave
   I'd like to say "Hello GET" with the StatusCode Ok for all my get requests
 *)
 
-let myApp = filter Get (OK "Hello GET")
+// let myApp = GET (OK "Hello GET")
+
+(* How about Hello POST *)
+let myApp = PUT (OK "Hello GET")
--- MiniSuave.fsx
+++ MiniSuave.fsx
@@ -1,11 +1,11 @@
 type StatusCode = Ok | NotFound | InternalError
-type HttpMethod = Get
-
+type HttpMethod = Get | Post | Put
+ 
 type HttpRequest = {
   HttpMethod : HttpMethod
 }
 
-type HttpResult = {
+type HttpResponse = {
   Content : string
   StatusCode : StatusCode
 }
@@ -15,18 +15,20 @@ let response statusCode content = async {
 }
 
 let OK = response Ok
-
 let NOT_FOUND = response NotFound
-
 let INTERNAL_ERROR = response InternalError
 
-let filter request app = async {
-  match request with
-  | Get -> return! app
-}
-   
-
-let execute app = 
-  app
-  |> Async.RunSynchronously
-  |> printfn "%A"
+let filter f app request = 
+  match f request with
+  | true -> Some app
+  | _ -> None
+let GET = (fun request -> request.HttpMethod = Get)
+let PUT = (fun request -> request.HttpMethod = Put)
+let POST = (fun request -> request.HttpMethod = Post)
+let execute app req = 
+  match app req with
+  | Some res -> 
+    res 
+    |> Async.RunSynchronously
+    |> printfn "%A"
+  | None -> printfn "No response found"
